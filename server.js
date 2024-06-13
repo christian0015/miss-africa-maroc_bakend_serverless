b4702b71072464aaf0e9b5ca8b9dea31bfa057ff
Christian Tukunda <christiantukundastocklin@gmail.com>
2024-06-14 00:35:22 +0100

    Configured CORS and other fixes

--- server.js
+++ server.js
@@ -8,13 +8,23 @@ const voteApi = require('./api/vote');
 const successApi = require('./api/success');
 require('dotenv').config();
 const cors = require('cors');
-const path = require('path'); // Importez le module path ici
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Définir les options CORS avec des origines spécifiques autorisées
+const corsOptions = {
+  origin: 'http://localhost:3000', // Seule cette origine est autorisée
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  optionsSuccessStatus: 200 // Certaines anciennes navigateurs (IE11, divers SmartTVs) ne fonctionnent pas avec le statut 204
+};
+
+// Utilisation de bodyParser pour analyser les corps de requête en JSON
 app.use(bodyParser.json());
-app.use(cors());
+
+// Utilisation de CORS avec les options définies
+app.use(cors(corsOptions));
 
 // Connectez-vous à la base de données MongoDB
 connectToDatabase(process.env.MONGODB_URI)
@@ -25,18 +35,16 @@ connectToDatabase(process.env.MONGODB_URI)
     console.error('Error connecting to MongoDB:', err);
   });
 
-
-
-// Routes
+// Routes API
 app.use('/api/users', userRoutes);
 app.use('/api/payments', paymentRoutes);
 
-// // Routes
+// Autres routes spécifiques
 app.use('/users', userApi);
 app.use('/vote', voteApi);
 app.use('/success', successApi);
 
-// Servez les fichiers statiques depuis le dossier 'public'
+// Servir les fichiers statiques depuis le dossier 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route pour toutes les autres requêtes GET, renvoyant index.html
